217beec60e10fd54d14b17d338a0f694d0bc003e
nglsena0722c <nglsena0722@com2us.com>
2023-12-14 11:42:52 +0900

    Fix : Fix responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
           </div>
         </div>
       </header>
-      <div className="bg-black bg-star md:h-[620px] h-[330px] flex justify-center items-center md:px-[10px]">
+      <div className="bg-black bg-star md:h-[620px] h-[330px] flex justify-center items-center md:px-[20px]">
         <div className="w-[1180px] h-full flex flex-col md:gap-[80px] justify-center md:items-start items-center relative">
           <div className="flex justify-start font-sora-700 text-gradient md:text-start text-center md:mb-[0px] mb-[40px]">
             <span className="md:text-[65px] text-[26px] md:leading-[82px] leading-[35px]">
@@ -55,7 +55,7 @@ function App() {
           <div className="flex md:flex-row flex-col justify-start md:gap-[20px] text-white font-sora-500 md:text-[30px] text-[20px]">
             <a
               target="_blank"
-              rel="noreferrer"
+              rel="noopener noreferrer"
               href="https://zealy.io/c/xpla/questboard"
               className="flex md:leading-[38px] leading-[25px] justify-center items-center border-solid border-[1px] border-white px-[45px] py-[10px] rounded-[100px] hover:cursor-pointer md:mb-[0px] mb-[20px]"
             >
@@ -65,7 +65,7 @@ function App() {
           </div>
           <img
             src="/img/wallet.svg"
-            className="md:block hidden absolute left-[774px] bottom-[0px]"
+            className="block max-[1400px]:hidden absolute left-[774px] bottom-[0px]"
             alt="vault-wallet"
             width="506px"
             height="620px"
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -6,13 +6,13 @@ import Tx from "./Tx";
 const Main = () => {
   return (
     <div className="flex flex-col justify-center bg-white items-center">
-      <div className="md:w-[1180px] md:mt-[80px] mt-[25px] md:mb-[86px] mb-[24px] flex flex-col md:gap-[80px] justify-center items-center">
+      <div className="md:max-w-[1180px] md:w-full md:mt-[80px] mt-[25px] md:mb-[86px] mb-[24px] flex flex-col md:gap-[80px] justify-center items-center md:px-[0px] px-[20px] ">
         <Balance />
         <NFT />
         <Stake />
         <Tx />
       </div>
-      <div className="md:max-w-[937px] max-w-[354px] text-[#0080FF] md:mb-[70px] mb-[30px] font-pretendard md:text-[20px] md:leading-[24px] text-[12px] leading-[11px] md:w-full text-center">
+      <div className="md:max-w-[937px] max-w-[354px] text-[#0080FF] md:mb-[70px] mb-[30px] font-pretendard md:text-[20px] md:leading-[24px] text-[12px] leading-[11px] md:w-full text-center max-[1180px]:px-[20px] ">
         💡 If the information displayed on your dashboard is inaccurate, please
         refresh your browser to update it.
       </div>
