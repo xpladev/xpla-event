7cb049a82f9f44e37f8c78b7d157e69f49a07e27
nglsena0722c <nglsena0722@com2us.com>
2024-04-18 11:09:23 +0900

    Fix : Fix sidebar for responsive design

--- src/components/PolicyComponents/PolicySidebar.tsx
+++ src/components/PolicyComponents/PolicySidebar.tsx
@@ -2,30 +2,30 @@ import clsx from "clsx";
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
-const LeftSidebar = () => {
+const PolicySidebar = () => {
     const location = useLocation();
     const { pathname } = location;
 
-    return <div className="mr-[39px] w-[236px] mb-[20px] md:mb-0">
+    return <div className="flex flex-col md:flex-row">
         <Link
-            className={clsx("text-[16px] font-medium leading-[19px]",
+            className={clsx("text-[16px] font-medium leading-[19px] mr-[12px]",
                 pathname === "/privacy" ? "text-[#00B1FF]" : "text-[#878D96]"
             )}
             to="/privacy"
         >
             Privacy Policy
         </Link>
-        <div className="w-full mt-[11px] mb-[10px] border border-[0.7px] border-t-0 border-[#878D96]" />
+        <div className="md:hidden w-full mt-[11px] mb-[10px] border border-[0.7px] border-t-0 border-[#878D96]" />
+        <div className="hidden md:block border-r-[0.7px]  border-[#878D96] mr-[12px]" />
         <Link
-            className={clsx("text-[16px] font-medium leading-[19px]",
+            className={clsx("text-[16px] font-medium leading-[19px] ",
                 pathname === "/cookie" ? "text-[#00B1FF]" : "text-[#878D96]"
             )}
             to="/cookie"
         >
             Cookie Policy
         </Link>
-        <div className="w-full mt-[11px] border border-[0.7px] border-t-0 border-[#878D96]" />
     </div>
 }
 
-export default LeftSidebar;
+export default PolicySidebar;
