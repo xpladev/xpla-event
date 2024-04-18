6e3a840b825942d099ac9a5e6314676f46a1cc6b
nglsena0722c <nglsena0722@com2us.com>
2024-04-18 14:04:34 +0900

    Fix : Fix css

--- src/Privacy.tsx
+++ src/Privacy.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "./components/PolicyComponents/Header";
-import LeftSidebar from "./components/PolicyComponents/LeftSidebar";
+import PolicySidebar from "./components/PolicyComponents/PolicySidebar";
 import Content from "./components/PolicyComponents/Content";
 import privacyData from "./privacy.json";
 import TopButton from "./components/PolicyComponents/TopButton";
@@ -13,9 +13,11 @@ const Privacy = () => {
     return <>
         <Header />
         <div className="w-full pt-[80px] flex justify-center items-start">
-            <div className="w-full max-w-[1920px] pl-[20px] md:pl-[40px] pt-[20px] md:pt-[50px] pb-[200px] pr-[35px] flex md:flex-row flex-col ">
-                <LanguageSelect classname="mb-[20px] block md:hidden"/>
-                <LeftSidebar />
+            <div className="w-full max-w-[1920px] pl-[40px] pt-[30px] pb-[200px] pr-[35px] flex flex-col justify-start items-center">
+                <div className="mb-[30px] w-full max-w-[1290px] flex justify-start items-start relative">
+                    <PolicySidebar />
+                    <LanguageSelect classname="" />
+                </div>
                 <Content
                     title="Privacy Policy"
                     policiesData={privacyData}
